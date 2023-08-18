ced730983cd03887d737bc475d714a6707143aab
Arun Kumar <aruncode99@gmail.com>
2023-08-18 15:42:27 +0530

    read file from file system

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+// Node js Native module -> like fs, http, https ..
+
+// 1. fs => file system
 const fs = require("fs");
 
 fs.writeFile("./message.txt", "Hello this is msg from node js", (err) => {
   if (err) throw err;
   console.log("The file has been saved!");
+});
+
+fs.readFile("./message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
 });
